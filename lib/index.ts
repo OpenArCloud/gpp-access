469cda9c460f1ebda725b9c63232d0c8ec40ad0c
Tamas Rigo <tamas.rigo@nokia.com>
2023-12-21 11:13:07 +0100

    refactor to use zod

--- lib/index.ts
+++ lib/index.ts
@@ -3,11 +3,6 @@
   This code is licensed under MIT license (see LICENSE.md for details)
 */
 
-import gppSchema from './geopose-request.schema.json';
-
-import Ajv from 'ajv';
-import addFormats from "ajv-formats"; // note: introduced in Ajv 8, see https://github.com/ajv-validator/ajv-formats
-
 import deepMerge from 'deepmerge';
 
 /** Endpoints for GeoPose services returned from Spatial Service Discovery */
@@ -16,14 +11,193 @@ export const objectEndpoint = 'scrs/geopose_objs';
 
 /** Base options for fetch to post json payload */
 const JSONPOST = {
-    method: "POST",
+    method: 'POST',
     headers: {
-        'Content-Type': 'application/json'
-    }
-}
+        'Content-Type': 'application/json',
+    },
+};
 
 let doValidation = true;
 
+import { z } from 'zod';
+
+export const cameraParamSchema = z.object({
+    model: z.string().optional(),
+    modelParams: z.array(z.number()).optional(),
+    minMaxDepth: z.array(z.number()).optional(),
+    minMaxDisparity: z.array(z.number()).optional(),
+});
+
+export const privacySchema = z.object({
+    dataRetention: z.array(z.string()),
+    dataAcceptableUse: z.array(z.string()),
+    dataSanitizationApplied: z.array(z.string()),
+    dataSanitizationRequested: z.array(z.string()),
+});
+
+export const imageOrientationSchema = z.object({
+    mirrored: z.boolean(),
+    rotation: z.number(),
+});
+
+export const cameraReadingSchema = z.object({
+    timestamp: z.number(),
+    sensorId: z.string(),
+    privacy: privacySchema,
+    sequenceNumber: z.number(),
+    imageFormat: z.string(),
+    size: z.array(z.number()),
+    imageBytes: z.string(),
+    imageOrientation: imageOrientationSchema.optional(),
+    params: cameraParamSchema.optional(),
+});
+
+export const geolocationReadingSchema = z.object({
+    timestamp: z.number(),
+    sensorId: z.string(),
+    privacy: privacySchema,
+    latitude: z.number(),
+    longitude: z.number(),
+    altitude: z.number(),
+    accuracy: z.number(),
+    altitudeAccuracy: z.number(),
+    heading: z.number(),
+    speed: z.number(),
+});
+
+export const wiFiReadingSchema = z.object({
+    timestamp: z.number(),
+    sensorId: z.string(),
+    privacy: privacySchema,
+    BSSID: z.string(),
+    frequency: z.number(),
+    RSSI: z.number(),
+    SSID: z.string(),
+    scanTimeStart: z.number(),
+    scanTimeEnd: z.number(),
+});
+
+export const bluetoothReadingSchema = z.object({
+    timestamp: z.number(),
+    sensorId: z.string(),
+    privacy: privacySchema,
+    address: z.string(),
+    RSSI: z.number(),
+    name: z.string(),
+});
+
+export const accelerometerReadingSchema = z.object({
+    timestamp: z.number(),
+    sensorId: z.string(),
+    privacy: privacySchema,
+    x: z.number(),
+    y: z.number(),
+    z: z.number(),
+});
+
+export const gyroscopeReadingSchema = z.object({
+    timestamp: z.number(),
+    sensorId: z.string(),
+    privacy: privacySchema,
+    x: z.number(),
+    y: z.number(),
+    z: z.number(),
+});
+
+export const magnetometerReadingSchema = z.object({
+    timestamp: z.number(),
+    sensorId: z.string(),
+    privacy: privacySchema,
+    x: z.number(),
+    y: z.number(),
+    z: z.number(),
+});
+
+export const positionSchema = z.object({
+    lon: z.number(),
+    lat: z.number(),
+    h: z.number(),
+});
+
+export const quaternionSchema = z.object({
+    x: z.number(),
+    y: z.number(),
+    z: z.number(),
+    w: z.number(),
+});
+
+export const vector3Schema = z.object({
+    x: z.number(),
+    y: z.number(),
+    z: z.number(),
+});
+
+export const sensorSchema = z.object({
+    id: z.string(),
+    name: z.string().optional(),
+    model: z.string().optional(),
+    rigIdentifier: z.string().optional(),
+    rigRotation: quaternionSchema.optional(),
+    rigTranslation: vector3Schema.optional(),
+    type: z.string(),
+});
+
+export const sensorReadingsSchema = z.object({
+    cameraReadings: z.array(cameraReadingSchema).optional(),
+    geolocationReadings: z.array(geolocationReadingSchema).optional(),
+    wiFiReadings: z.array(wiFiReadingSchema).optional(),
+    bluetoothReadings: z.array(bluetoothReadingSchema).optional(),
+    accelerometerReadings: z.array(accelerometerReadingSchema).optional(),
+    gyroscopeReadings: z.array(gyroscopeReadingSchema).optional(),
+    magnetometerReadings: z.array(magnetometerReadingSchema).optional(),
+});
+
+export const geoPoseAccuracySchema = z.object({
+    position: z.number(),
+    orientation: z.number(),
+});
+
+export const geoPoseSchema = z.object({
+    position: positionSchema,
+    quaternion: quaternionSchema,
+});
+
+export const geoPoseRespSchema = z.object({
+    id: z.string(),
+    timestamp: z.number(),
+    accuracy: geoPoseAccuracySchema,
+    type: z.string(),
+    geopose: geoPoseSchema,
+});
+
+export const geoPoseReqSchema = z.object({
+    id: z.string(),
+    timestamp: z.number(),
+    type: z.string(),
+    sensors: z.array(sensorSchema),
+    sensorReadings: sensorReadingsSchema,
+    priorPoses: z.array(geoPoseRespSchema).optional(),
+});
+
+export type GeoposeRequest = z.infer<typeof geoPoseReqSchema>;
+export type GeoposeResponse = z.infer<typeof geoPoseRespSchema>;
+export type Geopose = z.infer<typeof geoPoseSchema>;
+export type GeoposeAccuracy = z.infer<typeof geoPoseAccuracySchema>;
+export type SensorReadings = z.infer<typeof sensorReadingsSchema>;
+export type Sensor = z.infer<typeof sensorSchema>;
+export type Vector3 = z.infer<typeof vector3Schema>;
+export type Quaternion = z.infer<typeof quaternionSchema>;
+export type Position = z.infer<typeof positionSchema>;
+export type MagnetometerReading = z.infer<typeof magnetometerReadingSchema>;
+export type GyroscopeReading = z.infer<typeof gyroscopeReadingSchema>;
+export type AccelerometerReading = z.infer<typeof accelerometerReadingSchema>;
+export type BluetoothReading = z.infer<typeof bluetoothReadingSchema>;
+export type WiFiReading = z.infer<typeof wiFiReadingSchema>;
+export type GeolocationReading = z.infer<typeof geolocationReadingSchema>;
+export type CameraReading = z.infer<typeof cameraReadingSchema>;
+export type ImageOrientation = z.infer<typeof imageOrientationSchema>;
+export type Privacy = z.infer<typeof privacySchema>;
+export type CameraParam = z.infer<typeof cameraParamSchema>;
 
 /**
  * Convenience function to send GeoPose request to GeoPose service
@@ -35,22 +209,29 @@ let doValidation = true;
  * @param options  object  Additional options for the fetch. Default options will be overwritten.
  * @returns {Promise<Response>}  Response promise from GeoPose service, resolving to GeoPoseResponse json
  */
-export function sendRequest(serviceUrl, payload, options = {}) {
-    if (doValidation) validate(gppSchema, payload);
+export async function sendRequest(serviceUrl: string, payload: string, options: RequestInit = {}): Promise<GeoposeResponse> {
+    if (doValidation) {
+        validate(geoPoseReqSchema, payload);
+    }
 
-    let allOptions = deepMerge(JSONPOST, options);
+    const allOptions = deepMerge(JSONPOST, options);
     allOptions.body = payload;
 
-    return fetch(serviceUrl, allOptions)
-        .then(response => {
-            if (!response.ok) {
-                return response.text().then(
-                    text => { 
-                        throw new Error('Network response was not OK: ' + text) 
-                    })
-            }
-            return response.json();
-        })
+    const response = await fetch(serviceUrl, allOptions);
+    if (!response.ok) {
+        const responseText = await response.text();
+        throw new Error('Network response was not OK: ' + responseText);
+    }
+    const jsonResponse = await response.json();
+    if (doValidation) {
+        try {
+            const parsedResponse = geoPoseRespSchema.parse(jsonResponse);
+            return parsedResponse;
+        } catch (error) {
+            throw new Error(`Response does not conform to schema of GeoposeResponse: ${error}`);
+        }
+    }
+    return jsonResponse;
 }
 
 /**
@@ -60,22 +241,13 @@ export function sendRequest(serviceUrl, payload, options = {}) {
  * @param json  The json string to validate
  * @returns {boolean}  True when valid, throws an exception otherwise
  */
-export function validate(schema, json) {
-    let ajv = new Ajv();
-    addFormats(ajv);
-    let parsed;
-
+export function validate(schema: z.ZodSchema, json: string): boolean {
     try {
-        parsed = JSON.parse(scr);
+        schema.parse(JSON.parse(json));
+        return true;
     } catch (error) {
         throw new Error(`Unable to parse JSON content: ${error}`);
     }
-
-    if (ajv.validate(schema, parsed) === false) {
-        throw new Error(`Provided json isn't valid: ${ajv.errorsText()}`);
-    }
-
-    return true;
 }
 
 /**
@@ -87,6 +259,6 @@ export function validate(schema, json) {
  * @param validate  Boolean Indicates if payloads should be validated against geopose-request.schema
  *      before sending them to the server.
  */
-export function validateRequest(validate) {
+export function validateRequest(validate: boolean) {
     doValidation = validate;
 }
